3c957c44e702e582cb9f7371619ad761933ba123
felipeprata10 <felipeprata10@gmail.com>
2025-10-07 20:24:42 -0300

    Criado classe alunos

---
+++ app/src/main/java/com/example/myapplication/Alunos.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+public class Alunos {
+
+    String nomeAt;
+
+    String cursoAt;
+
+    public Alunos(String namePar, String coursePar){
+        this.nomeAt = namePar;
+        this.cursoAt = coursePar;
+
+    }
+
+    public String getNome(){
+        return nomeAt;
+    }
+    public String getCursoAt(){
+        return cursoAt;
+    }
+}
